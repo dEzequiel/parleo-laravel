0fd91247013bdb11525df6b20580a2398726d8fc
eze <91556738+dEzequiel@users.noreply.github.com>
2022-12-10 18:21:02 +0100

    Rename bc name conflicts

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+class Position extends Model
 {
     use HasFactory;
 
@@ -14,7 +14,7 @@ class Role extends Model
      *
      * @var string
      */
-    protected $table = 'role';
+    protected $table = 'position';
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +22,6 @@ class Role extends Model
      * @var array<int, string>
      */
     protected $fillable = [
-        'type'
+        'role'
     ];
 }
